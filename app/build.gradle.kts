65ef455bc8035f1a2466ede5911c30415faff0c4
vikasetrx <vikasetrx@gmail.com>
2025-05-11 12:46:49 +1200

    Upgrade AGP, other plugins and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,17 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
 }
 
 android {
     namespace = "nz.co.test.transactions"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "nz.co.test.transactions"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -47,10 +47,10 @@ android {
 dependencies {
 
     implementation(libs.dagger.core)
-    kapt(libs.dagger.compiler)
+    ksp(libs.dagger.compiler)
     implementation(libs.dagger.android)
     implementation(libs.dagger.android.support)
-    kapt(libs.dagger.android.processor)
+    ksp(libs.dagger.android.processor)
 
     implementation(libs.retrofit.core)
     implementation(libs.retrofit.converter.moshi)
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    id("com.android.application") version "8.3.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.google.dagger.hilt.android") version "2.50" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.dagger) apply false
+    alias(libs.plugins.ksp) apply false
 }
